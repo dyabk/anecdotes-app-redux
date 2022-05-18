27ac4565910103a1771103b51b0019b6cc143e1e
dyabk <dzmitry.yabkevich@outlook.com>
2022-05-17 22:41:50 -0400

    Refactored the code rendering the anecdotes into the module with the <AnecdoteList /> component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 
-import { useSelector, useDispatch } from "react-redux";
-import { asObject, castVote, createAnecdote } from "./reducers/anecdoteReducer";
-
 const App = () => {
-  const anecdotes = useSelector((state) => state);
-  anecdotes.sort((a, b) => b.votes - a.votes);
-  const dispatch = useDispatch();
-
-  const vote = (id) => {
-    dispatch(castVote(id));
-  };
-
   return (
     <div>
       <h2>Anecdotes</h2>
       <AnecdoteForm />
-      {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      ))}
+      <AnecdoteList />
     </div>
   );
 };
