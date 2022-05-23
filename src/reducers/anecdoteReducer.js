c167d234148d68a1a26e29a889acad32a6c7302e
dyabk <dzmitry.yabkevich@outlook.com>
2022-05-23 05:15:52 -0400

    Modified the initialization of Redux store to happen with asynchronous action creation

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import anecdoteService from "../services/anecdotes";
 
 const anecdoteSlice = createSlice({
   name: "anecdotes",
@@ -29,6 +30,14 @@ const anecdoteSlice = createSlice({
   },
 });
 
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll();
+    dispatch(setAnecdotes(anecdotes));
+  };
+};
+
 export const { createAnecdote, castVote, appendAnecdote, setAnecdotes } =
   anecdoteSlice.actions;
+
 export default anecdoteSlice.reducer;
